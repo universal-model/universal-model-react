8ed779a0824f96c762e6797ba860eb457031a7bf
Petri Silen <petri.silen@silensoft.com>
2020-01-26 12:21:33 +0200

    Modified

--- src/createStore.ts
+++ src/createStore.ts
@@ -7,7 +7,7 @@ export default function createStore<T extends State, U extends SelectorsBase<T>>
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   Object.entries(initialState).forEach(([, subState]: [string, any]) => {
     if (!Object.getOwnPropertySymbols(subState)[0]) {
-      throw new Error('useState: One of given subStates is not subState');
+      throw new Error('createStore: One of given subStates is not subState');
     }
   });
 
