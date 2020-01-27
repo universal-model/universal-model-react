c7cc63b5c6ff3c0c6eca85d7869461af813c2c53
Petri Silen <petri.silen@silensoft.com>
2020-01-27 09:35:05 +0200

    Modified

--- src/createStore.ts
+++ src/createStore.ts
@@ -1,11 +1,11 @@
-import Store, { Selectors, SelectorsBase, State } from './Store';
+import Store, { Selectors, SelectorsBase, State, SubState } from './Store';
 
 export default function createStore<T extends State, U extends SelectorsBase<T>>(
   initialState: T,
   selectors: Selectors<T, U>
 ): Store<T, U> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  Object.entries(initialState).forEach(([, subState]: [string, any]) => {
+  Object.entries(initialState).forEach(([, subState]: [string, SubState]) => {
     if (!Object.getOwnPropertySymbols(subState)[0]) {
       throw new Error(
         'createStore: One of given subStates is not subState. You must call createSubState(subState)'
